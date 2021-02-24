964f7775d648e770d7dbd5bfb71218b736b9b296
Thiago Santos <tisantos.ext@vr.com.br>
2021-02-24 03:01:53 -0300

    feat: Função do contador.

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../Button';
 
 import { Container } from './styles';
 
 const Countdown: React.FC = () => {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setActive(!active);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } 
+  }, [active, time]);
+
   return (
     <Container>
       <div className="countdown__timer-container">
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
 
-      <Button type="button">Iniciar um ciclo</Button>
+      <Button type="button" onClick={startCountdown}>{active ? 'Pausar o' : 'Iniciar um'} ciclo</Button>
     </Container>
   );
 }
